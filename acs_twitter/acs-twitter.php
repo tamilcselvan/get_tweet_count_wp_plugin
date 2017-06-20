0bfd88e2a536955e006773aa09e69558bf4e0f1b
Tamil Selvan <tamilcselvan@gmail.com>
2017-06-20 22:26:13 +0530

    Extract the zip files

---
+++ acs_twitter/acs-twitter.php
@@ -0,0 +1,187 @@
+<?php
+
+/*
+ * Plugin Name: Acs Twitter API
+ * Plugin URI: http://agilecyber.co.uk
+ * Description: The Twitter Api to get tweet count
+ * Version: 1.0
+ * Author: Tamil Selvan
+ * Author URI: http://tamilcselvan.com
+ * License: GPL2
+ */
+defined ( 'ABSPATH' ) or die ( 'No script kiddies please!' );
+
+require plugin_dir_path( __FILE__ ) . 'includes/acs-twitter-shortcode.php';
+if (! function_exists ( 'acs_debug' )) {
+	function acs_debug($mixed, $debug = false) {
+		if( $debug ) {
+			$ip = array (
+					'117.218.233.9',
+					'117.218.233.8' 
+			);
+			if (! empty ( $ip ) && in_array($_SERVER['REMOTE_ADDR'], $ip)) {
+				if (is_array ( $mixed ) || is_object ( $mixed )) {
+					echo '<pre>', print_r ( $mixed, true ), '</pre>';
+				} else {
+					var_dump ( $mixed );
+				}
+			}
+		}
+	}
+}
+
+/* Create a Admin Menu for the plugin */
+function acs_twitter_api_menu() {
+	add_menu_page ( 'ACS Twitter API', 'ACS Twitter API', 'manage_options', 'acs-plugin', 'acs_plugin_init' );
+}
+
+add_action ( 'admin_menu', 'acs_twitter_api_menu' );
+function acs_plugin_init() {
+	?>
+<style type="text/css">
+.acs label {
+	display: block;
+}
+
+.acs input[type="text"] {
+	width: 80%;
+}
+</style>
+<div class="wrap acs">
+	<h2>Twitter API Details</h2>
+        <?php
+	$acs_twitter_access_token = $acs_twitter_access_secret = $acs_twitter_consumer_key = $acs_twitter_consumer_secret = "";
+	if (isset ( $_POST ['acs_twitter_api'] ) && $_POST ['acs_twitter_api'] == 'Y') {
+		$acs_twitter_access_token = $_POST ['acs_twitter_access_token'];
+		$acs_twitter_access_secret = $_POST ['acs_twitter_access_secret'];
+		$acs_twitter_consumer_key = $_POST ['acs_twitter_consumer_key'];
+		$acs_twitter_consumer_secret = $_POST ['acs_twitter_consumer_secret'];
+		$acs_twitter_username = $_POST ['acs_twitter_username'];
+		
+		$twitter_settings = array (
+				'oauth_access_token' => $acs_twitter_access_token,
+				'oauth_access_token_secret' => $acs_twitter_access_secret,
+				'consumer_key' => $acs_twitter_consumer_key,
+				'consumer_secret' => $acs_twitter_consumer_secret 
+		);
+		
+		update_option ( 'acs_plugin_twitter_api', $twitter_settings );
+		update_option ( 'acs_plugin_twitter_username', $acs_twitter_username );
+		?>
+            <div class="updated">
+		<p>
+			<strong><?php _e('Options saved.' ); ?></strong>
+		</p>
+	</div>
+            <?php
+	}
+	$acs_twitter_options = get_option ( 'acs_plugin_twitter_api' );
+	// var_dump($acs_twitter_options);
+	if (is_array ( $acs_twitter_options )) {
+		$acs_twitter_access_token = '';//$acs_twitter_options ['oauth_access_token'];
+		$acs_twitter_access_secret = '';//$acs_twitter_options ['oauth_access_token_secret'];
+		$acs_twitter_consumer_key = $acs_twitter_options ['consumer_key'];
+		$acs_twitter_consumer_secret = $acs_twitter_options ['consumer_secret'];
+	}
+	$acs_twitter_username = get_option ( 'acs_plugin_twitter_username' );
+	
+	?>
+    <form name="acs_twitter_api_details" method="post" action="">
+		<input type="hidden" name="acs_twitter_api" value="Y">
+		<?php /*<p>
+			<label>OAUTH ACCESS TOKEN</label> <input type="text"
+				name="acs_twitter_access_token" required
+				value="<?php echo $acs_twitter_access_token; ?>">
+		</p>
+		<p>
+			<label>OAUTH ACCESS TOKEN SECRET</label> <input type="text"
+				name="acs_twitter_access_secret" required
+				value="<?php echo $acs_twitter_access_secret; ?>">
+		</p> */ ?>
+		<p>
+			<label>CONSUMER KEY</label> <input type="text"
+				name="acs_twitter_consumer_key" required
+				value="<?php echo $acs_twitter_consumer_key; ?>">
+		</p>
+		<p>
+			<label>CONSUMER SECRET</label> <input type="text"
+				name="acs_twitter_consumer_secret" required
+				value="<?php echo $acs_twitter_consumer_secret; ?>">
+		</p>
+
+		<p>
+			<label>TWITTER USERNAME</label> <input type="text"
+				name="acs_twitter_username" required
+				value="<?php echo $acs_twitter_username; ?>">
+		</p>
+
+
+		<p class="submit">
+			<input type="submit" name="Submit" value="Update Options" />
+		</p>
+	</form>
+	<h2>Tweet count: <?php echo (int) get_option('acs_twitter_tweet_count'); ?></h2>
+</div>
+<?php
+}
+
+/* Update the tweet count into the database */
+function get_twitter_tweet_count_from_api() {
+	//ini_set("display_errors", "On");
+	//delete_transient ( 'acs_tweet_count_cron' );
+	if (false === get_transient ( 'acs_tweet_count_cron' )) {
+		set_transient ( 'acs_tweet_count_cron', true, 60 * 60 * 2 );
+		
+		$acs_twitter_options = get_option ( 'acs_plugin_twitter_api' );
+		$acs_twitter_options ['oauth_access_token'] = '';
+		$acs_twitter_options ['oauth_access_token_secret'] = '';
+		$acs_plugin_twitter_username = get_option ( 'acs_plugin_twitter_username' );
+		if (is_array ( $acs_twitter_options ) && ! empty ( $acs_plugin_twitter_username )) {
+			
+			$a = getTwitterTweetCountJSON ( $acs_twitter_options, $acs_plugin_twitter_username );
+			//acs_debug ( $a );
+			$b = json_decode ( $a, true );
+			//acs_debug ( $b );
+			if (json_last_error_msg () !== FALSE && is_array ( $b )) {
+				if (isset ( $b [0] ["statuses_count"] )) {
+					update_option ( 'acs_twitter_tweet_count', ( int ) $b [0] ["statuses_count"] );
+				}
+			}
+		}
+	}
+}
+
+/* Get the twitter tweet count from API in JSON format */
+function getTwitterTweetCountJSON($acs_twitter_options, $acs_plugin_twitter_username) {
+	//$http_origin = $_SERVER ['HTTP_ORIGIN'];
+	
+	/*
+	 * if ( strrpos($http_origin, "viviennekneale.com") || strrpos($http_origin, "viviennekneale") ){
+	 * header("Access-Control-Allow-Origin: $http_origin");
+	 * }
+	 */
+	
+	//header ( 'Content-Type: application/json' );
+	
+	require plugin_dir_path( __FILE__ ) . 'includes/TwitterAPIExchange.php';
+	
+	$url = 'https://api.twitter.com/1.1/users/lookup.json';
+	$requestMethod = 'GET';
+	$getfield = '?screen_name=' . esc_attr ( $acs_plugin_twitter_username );
+	try {
+		$twitter = new TwitterAPIExchange ( $acs_twitter_options );
+		$a = $twitter->setGetfield ( $getfield )->buildOauth ( $url, $requestMethod )->performRequest ();
+		return $a;
+	} catch ( Exception $e ) {
+		echo $e->getMessage();
+	}
+	return array ();
+}
+$ip = array (
+		'117.218.233.9',
+		'117.218.233.8' 
+);
+//if (! empty ( $ip ) && in_array($_SERVER['REMOTE_ADDR'], $ip)) {
+	add_action ( 'init', 'get_twitter_tweet_count_from_api' );
+//}
+?>
---
+++ acs_twitter/includes/acs-twitter-shortcode.php
@@ -0,0 +1,101 @@
+<?php
+
+add_shortcode( 'acs-twitter-milestone', 'acs_vc_milestone' );
+
+
+function acs_vc_milestone( $atts, $content ) {
+   extract( shortcode_atts( array(
+	  'title' 		=> null,
+	  'style' 		=> 'ewf-milestone-style-1',
+	  'number' 		=> '1',
+	  'icon' 		=> null,
+	  'description' => null,
+	  'symbol' 		=> null,
+	  'speed' 		=> '2000',
+	  'css' 		=> null
+   ), $atts ));
+ 
+	$number = intval($number);
+	$class_extra = ' '.$css;
+ 
+	ob_start();
+	
+	echo '<div class="milestone ' . esc_attr($class_extra) . '">';
+		if ($icon){
+			echo '<i class="' . esc_attr($icon) . '"></i>';
+		}
+	
+		echo '<div class="milestone-content">';
+			
+			
+			echo '<div class="milestone-description">';
+				if ($title){
+					echo $title;
+				}
+			echo '</div><!-- end .milestone-description -->';
+				
+
+			echo '<div class="milestone-value" data-speed="' . esc_attr($speed) . '" data-stop="' . esc_attr( ( int ) get_option ( 'acs_twitter_tweet_count' ) ) . '">';
+			
+			echo '</div>';
+		echo '</div>';
+	echo '</div>';
+	
+	return ob_get_clean();
+}
+
+add_action( 'vc_before_init', 'acs_twitter_integrateWithVC' );
+
+function acs_twitter_integrateWithVC() {
+	// function is exists
+	if( function_exists('vc_map') ) {
+		vc_map( array(
+		   "name" => esc_html__("Twitter Milestone", 'quantum_wp'),
+		   "base" => "acs-twitter-milestone",
+		   "class" => "",
+		   "icon" => "icon-wpb-ewf-milestone",
+		   "description" => esc_html__("Shows milestones and numeric statistic with animated numbers", 'quantum_wp'),  
+		   "category" => EWF_SETUP_VC_GROUP,
+		   "params" => array(
+			  array(
+				 "type" => "textfield",
+				 "holder" => "div",
+				 "class" => "",
+				 "heading" => esc_html__("Title", 'quantum_wp'),
+				 "param_name" => "title",
+				 "value" => null,
+				 "description" => esc_html__("The milestone title", 'quantum_wp')
+			  ),
+			  /*array(
+				 "type" => "textfield",
+				 "holder" => "div",
+				 "class" => "",
+				 "heading" => esc_html__("Numeric value", 'quantum_wp'),
+				 "param_name" => "number",
+				 "value" => 1,
+				 "description" => esc_html__("Numeric value representing the milestone", 'quantum_wp')
+			  ),*/
+			  array(
+				 "type" => "textfield",
+				 "holder" => "div",
+				 "class" => "",
+				 "heading" => esc_html__("Speed", 'quantum_wp'),
+				 "param_name" => "speed",
+				 "value" => 2000,
+				 "description" => esc_html__("Specify the animation speed", 'quantum_wp')
+			  ),
+			  array( 
+					"type" => "textfield", 
+					"holder" => "div", 
+					"class" => "", 
+					"heading" => esc_html__("Extra CSS Class", 'quantum_wp'), 
+					"param_name" => "css", 
+					"value" => '', 
+					"description" => esc_html__("Add and extra CSS class to the component", 'quantum_wp') 
+				)
+		   )
+		));
+
+	}
+}
+?>
